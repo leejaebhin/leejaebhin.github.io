fc474bf4b135ff58ff37218d6a2b764d2c008964
leejaebhin <100827435+leejaebhin@users.noreply.github.com>
2022-04-07 04:12:19 +0900

    Add files via upload

---
+++ php.homework4/hw4-4.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    $n = $_GET['i'];
+
+    for ($i = 1; $i <= $n; $i++) { 
+        echo ("($i 번째 숫자 : " . $dada['i']=rand(10,100) . ")");
+    }
+    
+?>
+</body>
+</html>
